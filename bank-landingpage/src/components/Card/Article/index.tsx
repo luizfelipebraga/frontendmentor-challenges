0e0b8de3b1160e33b9cbe5e6bd57a71f2de5d1c8
Luiz Felipe Braga <ruivolfb@gmail.com>
2022-02-07 18:00:52 -0300

    some things

--- bank-landingpage/src/components/Card/Article/index.tsx
+++ bank-landingpage/src/components/Card/Article/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Container, Title, Description, Box, MadeBy } from "./styles";
+import { Container, Title, Description, Box, MadeBy, ImageStyled } from "./styles";
 
 type CardProps = {
   img: string;
@@ -11,7 +11,7 @@ type CardProps = {
 export function CardArticle({ img, title, description, madeBy}: CardProps) {
   return (
     <Container>
-      <Image src={img} width={350} height={300}/>
+      <ImageStyled src={img} width={350} height={300}/>
       <Box>
         <MadeBy>{madeBy}</MadeBy>
         <Title>{title}</Title>
--- bank-landingpage/src/components/Card/Article/styles.ts
+++ bank-landingpage/src/components/Card/Article/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import Image from 'next/image';
+
 
 export const Container = styled.div`
   background-color: #fff;
@@ -11,6 +13,10 @@ export const Box = styled.div`
   gap: 2rem;
 
   margin-top: 2rem;
+`;
+
+export const ImageStyled = styled(Image)`
+  border-radius: .3rem !important;
 `;
 
 export const MadeBy = styled.span`
